d0fdf54b0e2b00090459f6781f77a0b34e38e0ed
Felix Gourdeau <felix.gourdeau@quebecormedia.com>
2022-04-01 21:45:34 -0700

    trigger workflow

--- scripts/prepare.js
+++ scripts/prepare.js
@@ -1,12 +1,16 @@
 const fs = require("fs");
 const path = require("path");
+const pkg = require("../package.json");
+
+// Remove unnecessary properties from package.json
+delete pkg.scripts;
+delete pkg.devDependencies;
 
 // Copy package.json to the dist folder
-fs.copyFileSync(
-  path.resolve(__dirname, "../package.json"),
-  path.resolve(__dirname, "../dist/package.json")
+fs.writeFileSync(
+  path.resolve(__dirname, "../dist/package.json"),
+  JSON.stringify(pkg, null, 2)
 );
-
 // Copy README to the dist folder
 fs.copyFileSync(
   path.resolve(__dirname, "../README.md"),
